92f4246f2401484c97dbd3acf227a4fdfe1dde92
Celal Kaan Aytemir <ck.aytemir@gmail.com>
2010-11-05 21:24:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EersteApp::Application.config.secret_token = 'ffb23f630ac41698261858d94567a458444801b36196da1a864752e991fd65cf83f1f5439cb349cfaf31d683e6ef8d60b6b7cd866a8fc553759ca092195d0237'
